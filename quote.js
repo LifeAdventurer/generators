6a6aaf56723a8e6b5c1778e564c35fc0ea200385
Moon <108756201+LifeAdventurer@users.noreply.github.com>
2023-10-03 20:57:50 +0800

    Update quote.js replace listing quotes by fetching a json file

--- quote.js
+++ quote.js
@@ -2,38 +2,23 @@ const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
 const buttonElement = document.querySelector("button");
 
-const quotes = [
-  {
-    quote: "To AC is human. To AK divine.",
-    author: "Moon"
-  },
-  {
-    quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
-    author: "Albert Einstein"
-  },
-  {
-    quote: "A dream is what makes people love life even when it is painful.",
-    author: "Theodore Zeldin"
-  },
-  {
-    quote: "Don’t quit. Suffer now and live the rest of your life as a champion.",
-    author: "Muhammad Ali"
-  },
-];
+let quotes = [];
+
+fetch("quotes.json")
+.then(response => response.json())
+.then(data => {
+  quotes = data.quotes;
+});
+
 
 function Appear() {
+  console.log(quotes);
   const index = Math.floor(Math.random() * quotes.length);
-  const { quote, author } = quotes[index];
-  console.log(index)
-  quoteElement.innerHTML = `<b style='font-size:16px;'>"</b>` + quote + `<b style='font-size:16px;'>"</b>` ;
+  const {quote, author} = quotes[index];
+  quoteElement.innerHTML = `<b style='font-size:28px;'>"</b>` + quote + `<b style='font-size:28px;'>"</b>` ;
   authorElement.innerHTML = "- " + author;
 }
 
 function getQuote() {
-  let a = parseInt(Math.random() * 255);
-  let str = `rgba(${a}, ${Math.abs(a - 127)}, ${Math.abs(a - 255)}, 0.55)`;
-  console.log(str);
-  Update(str);
+  Update();
 }
-
-getQuote();
