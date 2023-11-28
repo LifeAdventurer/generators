74bddb89f2e14c1f83a715b221a05511b1b8687b
lifeadventurer <life0adventurer@gmail.com>
2023-11-28 16:57:30 +0800

    refactor: remove prefix and suffix of generate_quote_json.cpp

--- quote_generator/generate_quotes_json.cpp
+++ quote_generator/generate_quotes_json.cpp
@@ -1,15 +1,3 @@
-/**              _         ___                       _
- *              | |    _  /  _|                     | |                        _
- *              | |   |_| | |  _____     ____    ___| |__    __ _____  _____  | |   _   _  ____  _____  ____
- *    author:   | |    _ [   ]/  _  \   /  _ \  /  _  |\ \  / //  _  \|  _  \[   ] | | | ||  __|/  _  \|  __|
- *              | L__ | | | | | ____|   | |_\ \ | |_| | \ \/ / | ____|| | | | | |_ | |_| || /   | ____|| /
- *              L____||_| |_| \_____|   \____\_\\_____/  \__/  \_____||_| |_| |___|\_____/|_|   \_____||_|
- *
- *    Life is an adventure. Be adventurous.
- *
- *    created: 02.10.2023 01:14:44
- *
-**/
 #include <iostream>
 #include <string>
 
@@ -32,9 +20,3 @@ int main() {
 
   return 0;
 }
-
-/**
- *        "To stop trying is never the solution."
- *        "You are not alone. Lord have your way."
- *        "Failure is another blessing. Only by accepting it calmly can it bring growth."
-**/
