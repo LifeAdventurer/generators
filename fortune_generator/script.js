c818817a3f7a9d6f0241a8c8977c0ed88e9b2a80
lifeadventurer <life0adventurer@gmail.com>
2023-12-23 14:00:47 +0800

    feat: add dark mode for fortune generator

---
+++ fortune_generator/script.js
@@ -0,0 +1,7 @@
+let darkModeIcon = document.querySelector('#dark-mode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+  
