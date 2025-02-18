0b31ee45a802e5f200e3c7ee98f5e482584ec3d7
tobiichi3227 <cz1346219@gmail.com>
2025-02-18 10:44:10 +0800

    Chore(Fortune): replace shell script with Python for better compatibility

--- scripts/check-events.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-python3 scripts/check-events.py fortune_generator/json/custom_special.json custom
-python3 scripts/check-events.py fortune_generator/json/cyclical_special.json cyclical
-python3 scripts/check-events.py fortune_generator/json/static_special.json static
